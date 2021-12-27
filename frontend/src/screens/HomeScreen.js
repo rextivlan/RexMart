bcc4c227cc34df4697383c9fb4ae52ec39748bcb
rextivlan <affan.ali368@gmail.com>
2021-12-27 15:00:57 +0530

    Add Message, Loader Components and Product Details Reducer, Actions

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Product";
 import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
@@ -16,11 +18,11 @@ const HomeScreen = () => {
 
   return (
     <>
-      <h1>Latest Productus</h1>
+      <h1>Our Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
