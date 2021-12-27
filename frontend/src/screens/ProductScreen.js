8a3c02a95754a7ce3b55201060dd4abbe7b3ef44
rextivlan <affan.ali368@gmail.com>
2021-12-27 14:23:30 +0530

    Implement Redux by adding Reducers, Actions

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -18,7 +18,7 @@ const ProductScreen = () => {
 
   useEffect(() => {
     const fetchProduct = async () => {
-      const { data } = await axios.get(`/products/getProduct/${id}`);
+      const { data } = await axios.get(`/api/products/${id}`);
 
       setProduct(data);
     };
