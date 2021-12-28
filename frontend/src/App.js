be5ebd614248385ee9ad2a388d46740f467fe774
rextivlan <affan.ali368@gmail.com>
2021-12-28 14:14:12 +0530

    Add Shoopping Cart and fix bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 const App = () => {
   return (
@@ -15,6 +16,11 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
             <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart">
+              <Route path=":id/:qty" element={<CartScreen />} />
+              <Route path="" element={<CartScreen />} />
+            </Route>
+            {/* <Route path="/cart/:id/:qty" exact element={<CartScreen />} /> */}
           </Routes>
         </Container>
       </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,21 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const intialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const intialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
