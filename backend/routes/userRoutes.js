1d067e8a37d282f9543f8f69f5df90c6af582182
rextivlan <affan.ali368@gmail.com>
2022-01-15 19:23:43 +0530

    Add Endpoints for getting and updating user by id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,12 +7,18 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from "../controllers/userController.js";
 import { auth, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser).get(auth, admin, getUsers);
 router.post("/login", authUser);
 router.route("/profile").get(auth, getUserProfile).put(auth, updateUserProfile);
-router.route("/:id").delete(auth, admin, deleteUser);
+router
+  .route("/:id")
+  .delete(auth, admin, deleteUser)
+  .get(auth, admin, getUserById)
+  .put(auth, admin, updateUser);
 
 export default router;
