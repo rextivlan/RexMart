07cbcba3c3e2cbb9aa24374c449235076b7848fa
rextivlan <affan.ali368@gmail.com>
2022-01-22 23:59:29 +0530

    Add Product Carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,11 +7,13 @@ import {
   updateProduct,
   createProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { auth, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(auth, admin, createProduct);
 router.route("/:id/reviews").post(auth, createProductReview);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
